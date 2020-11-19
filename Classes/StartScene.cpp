ad099477ad6b661dda29196d156c250c17998243
jackyeightzhang <10355487+jackyEightz@users.noreply.github.com>
2020-11-19 11:52:47 -0800

    break down start scene element initialization into inividual functions

--- Classes/StartScene.cpp
+++ Classes/StartScene.cpp
@@ -3,7 +3,7 @@
 //
 // Author: Jacky Zhang (jackyeightzhang)
 // Date created: 18 Nov 2020
-// Date last modified: 18 Nov 2020
+// Date last modified: 19 Nov 2020
 //
 
 #include "StartScene.h"
@@ -21,50 +21,45 @@ static void problemLoading(const char* filename)
     printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
 }
 
-bool Start::init() {
+bool Start::initPlayButton(const Vec2& origin, const Size& visibleSize) {
+    // create button
+    auto playButton = ui::Button::create("spin_button.png",
+                                                 "spin_button.png",
+                                                 "spin_button.png");
 
-    // 1. initialize Scene supercslass
-    if(!Scene::init()) {
-        return false;
-    }
+    // setting button size
+    playButton->setScale(MAX(playButton->getContentSize().width / visibleSize.width,
+                             playButton->getContentSize().height / visibleSize.height));
+    // setting button text
+    playButton->setTitleAlignment(TextHAlignment::CENTER);
+    playButton->setTitleText("Play On");
 
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
-    // 2. add a menu button, which is clicked to go to end scene and application
-    // auto playButtonMenuItem = MenuItemImage::create("spin_button.png", "spin_button.png", CC_CALLBACK_1());
-    auto playButtonMenuItem = ui::Button::create("spin_button.png",
-                                                          "spin_button.png",
-                                                          "spin_button.png");
-    playButtonMenuItem->setTitleAlignment(TextHAlignment::CENTER);
-    playButtonMenuItem->setTitleText("Play On");
-//    playButtonMenuItem->setPressedActionEnabled(true);
-//    playButtonMenuItem->setZoomScale(-0.5);
-    playButtonMenuItem->addTouchEventListener(CC_CALLBACK_1(Start::menuCloseCallback,this));
-    playButtonMenuItem->setScale(MAX(playButtonMenuItem->getContentSize().width / visibleSize.width,
-                                     playButtonMenuItem->getContentSize().height / visibleSize.height));
-
+    // setting button functionality
+    playButton->addTouchEventListener(CC_CALLBACK_1(Start::spinSceneCallback,this));
+//    playButton->setPressedActionEnabled(true);
+//    playButton->setZoomScale(-0.5);
 
     // check to see if play button does not load correctly
-    if (playButtonMenuItem == nullptr ||
-        playButtonMenuItem->getContentSize().width <= 0 ||
-        playButtonMenuItem->getContentSize().height <= 0) {
+    if (playButton == nullptr ||
+        playButton->getContentSize().width <= 0 ||
+        playButton->getContentSize().height <= 0) {
         problemLoading("'Spin Button'");
     } else {
         float x = origin.x + visibleSize.width / 2;
         float y = origin.y + visibleSize.height / 5 * 1;
-        playButtonMenuItem->setPosition(Vec2(x,y));
+        playButton->setPosition(Vec2(x,y));
     }
 
-    // adding all the menu items to menu
-//    auto menu = Menu::create(playButtonMenuItem, NULL);
-//    menu->setPosition(Vec2::ZERO);
+    this->addChild(playButton, 0);
 
-    // 3. add wheel panel sprite
+    return true;
+}
+
+bool Start::initWheel(const Vec2& origin, const cocos2d::Size& visibleSize){
     auto wheelPanelSprite = Sprite::create("wheel_sections_8.png");
 
     float wheelScale = MAX(wheelPanelSprite->getContentSize().width / visibleSize.width,
-                     wheelPanelSprite->getContentSize().height / visibleSize.height);
+                           wheelPanelSprite->getContentSize().height / visibleSize.height);
 
     wheelPanelSprite->setScale(wheelScale);
 
@@ -122,15 +117,15 @@ bool Start::init() {
     auto hammer1Sprite_8 = Sprite::create("hammer.png");
 
     Vector<Sprite*> prizes = {
-                              life30Sprite_1,
-                              brush3Sprite_2,
-                              gems35Sprite_3,
-                              hammer3Sprite_4,
-                              coins750Sprite_5,
-                              brush1Sprite_6,
-                              gems75Sprite_7,
-                              hammer1Sprite_8
-                              };
+            life30Sprite_1,
+            brush3Sprite_2,
+            gems35Sprite_3,
+            hammer3Sprite_4,
+            coins750Sprite_5,
+            brush1Sprite_6,
+            gems75Sprite_7,
+            hammer1Sprite_8
+    };
 
     float ROT_DEGREE = 360/16;
     float PRIZE_SIZE = wheelScale * life30Sprite_1->getContentSize().height;
@@ -147,20 +142,20 @@ bool Start::init() {
             float y = origin.y + visibleSize.height / 5 * 3;
             sprite->setPosition(Vec2(x,y));
             sprite->setAnchorPoint(Vec2(0.5,-2.5));
-            sprite->setRotation(ROT_DEGREE * ( 1 + ( i * 2 ) ) );
+            sprite->setRotation( ROT_DEGREE * ( 1 + ( i * 2 ) ) );
             this->addChild(sprite,2);
         }
     }
 
     // 7. adding labels to all the prize sprites
-    auto life30Label_1 = Label::createWithSystemFont("30x\nmins","Arial", 12, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
-    auto brush3Label_2 = Label::createWithSystemFont("3x","Arial", 12, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
-    auto gems35Label_3 = Label::createWithSystemFont("35x","Arial", 12, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
-    auto hammer3Label_4 = Label::createWithSystemFont("3x","Arial", 12, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
-    auto coins750Label_5 = Label::createWithSystemFont("750x","Arial", 12, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
-    auto brush1Label_6 = Label::createWithSystemFont("1x","Arial", 12, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
-    auto gems75Label_7 = Label::createWithSystemFont("75x","Arial", 12, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
-    auto hammer1Label_8 = Label::createWithSystemFont("1x","Arial", 12, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
+    auto life30Label_1 = Label::createWithSystemFont("30x\nmins","Arial", 9, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
+    auto brush3Label_2 = Label::createWithSystemFont("3x","Arial", 9, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
+    auto gems35Label_3 = Label::createWithSystemFont("35x","Arial", 9, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
+    auto hammer3Label_4 = Label::createWithSystemFont("3x","Arial", 9, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
+    auto coins750Label_5 = Label::createWithSystemFont("750x","Arial", 9, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
+    auto brush1Label_6 = Label::createWithSystemFont("1x","Arial", 9, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
+    auto gems75Label_7 = Label::createWithSystemFont("75x","Arial", 9, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
+    auto hammer1Label_8 = Label::createWithSystemFont("1x","Arial", 9, Size(PRIZE_SIZE,PRIZE_SIZE),TextHAlignment::RIGHT,TextVAlignment::BOTTOM);
 
     Vector<Label*> amounts = {
             life30Label_1,
@@ -186,25 +181,40 @@ bool Start::init() {
             float y = origin.y + visibleSize.height / 5 * 3;
             label->setPosition(Vec2(x,y));
             label->setAnchorPoint(Vec2(0.5,-2.5));
-            label->setRotation(ROT_DEGREE * ( 1 + ( i * 2 ) ) );
+            label->setRotation(( ROT_DEGREE * ( 1 + ( i * 2 ) ) ) + 4 );
             this->addChild(label,2);
         }
     }
 
     // adding all the objects into the scene
-    this->addChild(playButtonMenuItem,0);
     this->addChild(wheelPanelSprite,1);
     this->addChild(wheelBorder,3);
     this->addChild(wheelArrow,4);
     return true;
 }
 
-void Start::menuCloseCallback(Ref* pSender)
+bool Start::init() {
+
+    // 1. initialize Scene supercslass
+    if(!Scene::init()) {
+        return false;
+    }
+
+
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+
+    // 2. add play button
+    initPlayButton(origin, visibleSize);
+
+    // 3. add wheel sprite
+    initWheel(origin, visibleSize);
+
+    return true;
+}
+
+void Start::spinSceneCallback(Ref* pSender)
 {
-    //Close the cocos2d-x game scene and quit the application
-    Director::getInstance()->end();
-
-    EventCustom customEndEvent("game_scene_close_event");
-    _eventDispatcher->dispatchEvent(&customEndEvent);
-
+    // auto spinScene =
+    // Director::getInstance()->replaceScene();
 }
--- Classes/StartScene.h
+++ Classes/StartScene.h
@@ -3,7 +3,7 @@
 //
 // Author: Jacky Zhang (jackyeightzhang)
 // Date created: 18 Nov 2020
-// Date last modified: 18 Nov 2020
+// Date last modified: 19 Nov 2020
 //
 
 #ifndef __START_SCENE_H__
@@ -15,10 +15,14 @@ class Start : public cocos2d::Scene {
 public:
     static cocos2d::Scene* createScene();
 
+    // helper functions for initializing scene
+    virtual bool initPlayButton(const cocos2d::Vec2& origin, const cocos2d::Size& visibleSize);
+    virtual bool initWheel(const cocos2d::Vec2& origin, const cocos2d::Size& visibleSize);
+
     virtual bool init();
 
     // a callback method for when Play is pressed
-    void menuCloseCallback(cocos2d::Ref* pSender);
+    void spinSceneCallback(cocos2d::Ref* pSender);
 
     CREATE_FUNC(Start);
 };
