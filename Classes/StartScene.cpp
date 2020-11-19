5c277c33bc62113c5c6f3fd7d5265c59a644e85e
jackyeightzhang <10355487+jackyEightz@users.noreply.github.com>
2020-11-18 22:44:48 -0800

    implemented wheel

--- Classes/StartScene.cpp
+++ Classes/StartScene.cpp
@@ -35,19 +35,44 @@ bool Start::init() {
                                                  "spin_button.png",
                                                  CC_CALLBACK_1(Start::menuCloseCallback,this)
                                                 );
-    playButtonMenuItem->setScale(playButtonMenuItem->getContentSize().width / visibleSize.width,
-                                 playButtonMenuItem->getContentSize().height / visibleSize.height);
+    playButtonMenuItem->setScale(MAX(playButtonMenuItem->getContentSize().width / visibleSize.width,
+                                           playButtonMenuItem->getContentSize().height / visibleSize.height));
+
 
     // check to see if play button does not load correctly
     if (playButtonMenuItem == nullptr ||
         playButtonMenuItem->getContentSize().width <= 0 ||
         playButtonMenuItem->getContentSize().height <= 0) {
-        problemLoading("'spin_button'");
+        problemLoading("'Spin Button'");
     } else {
         float x = origin.x + visibleSize.width / 2;
-        float y = origin.y + visibleSize.height / 5;
+        float y = origin.y + visibleSize.height / 5 * 1;
         playButtonMenuItem->setPosition(Vec2(x,y));
     }
+
+    // 3. add wheel panel sprite
+    auto wheelPanelSprite = Sprite::create("wheel_sections_8.png");
+
+    wheelPanelSprite->setScale(MAX(playButtonMenuItem->getContentSize().width / visibleSize.width,
+                                         playButtonMenuItem->getContentSize().height / visibleSize.height));
+
+    // check to see if play button does not load correctly
+    if (wheelPanelSprite == nullptr ||
+        wheelPanelSprite->getContentSize().width <= 0 ||
+        wheelPanelSprite->getContentSize().height <= 0) {
+        problemLoading("'wheel panel'");
+    } else {
+        float x = origin.x + visibleSize.width / 2;
+        float y = origin.y + visibleSize.height / 5 * 3;
+        wheelPanelSprite->setPosition(Vec2(x,y));
+    }
+
+    // 4. add wheel border sprite
+    auto wheelBorder = Sprite::create("wheel_border.png");
+
+
+    // 10. add wheel arrow, last because it is infront of the wheel border and
+    auto wheelArrow = Sprite::create("wheel_arrow.png");
 
     // adding all the menu items to menu
     auto menu = Menu::create(playButtonMenuItem, NULL);
@@ -55,7 +80,8 @@ bool Start::init() {
 
     // adding all the objects into the scene
     this->addChild(menu);
-
+    this->addChild(wheelPanelSprite);
+    // this->addChild(wheelBorder);
     return true;
 }
 
